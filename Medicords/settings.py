8baaece8c21b12f06aabcbe713c7f98f491925a1
PranavSheth12 <79102762+PranavSheth12@users.noreply.github.com>
2021-09-05 01:25:47 +0530

    Update settings.py

--- Medicords/settings.py
+++ Medicords/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c#%&eug(3a-ryrn38ek*6gc27p*5f7i@!5y*dfo*z_5zgj^=(2'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
